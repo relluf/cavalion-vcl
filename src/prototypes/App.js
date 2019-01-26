a5f793d73defd9864d364b54ac34f213f34de5ca
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-01-26 14:35:49 -0600

    refactoring styles to be as high up as possible

--- src/prototypes/App.js
+++ src/prototypes/App.js
@@ -11,5 +11,18 @@ $("vcl/Application", {
     		document.body);
 	}
 }, [
-    $("vcl/ui/Panel", "window", { align: "client", classes: "animated" }, [])
+    $("vcl/ui/Panel", "window", { 
+    	align: "client", 
+    	classes: "animated",
+		css: {
+			// "background": "-webkit-linear-gradient(top, rgb(252, 252, 252) 0%, rgb(255, 255, 255) 50%, #fafafa 100%)",
+			"font-family": "Lucida Grande, Arial, sans-serif",
+			"font-size": "9pt",
+			".{./Button}": {
+				"font-family": "Lucida Grande, Arial, sans-serif",
+				"font-size": "9pt"
+			}
+		}
+    
+    }, [])
 ]);
