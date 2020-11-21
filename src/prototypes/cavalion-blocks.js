1b3c75658f908d70e225c39e7c697229b8a8e89a
Ralph Kazemier <ralph.kazemier@gmail.com>
2020-11-20 19:19:22 -0600

    updating for veldoffice-publiek-putten

--- src/prototypes/cavalion-blocks.js
+++ src/prototypes/cavalion-blocks.js
@@ -1,7 +1,9 @@
+"use B";
+
 $("vcl/ui/Panel", {
 	align: "client",
 	onLoad() {
-		B.instantiate(js.sf("%s<%s>", this.getSpecializer(), this.vars("specializer") || ""), 
+		require("B").instantiate(js.sf("%s<%s>", this.getSpecializer(), this.vars("specializer") || ""), 
 			{ owner: this }).then(_ => {
 				_.setParent(this); // TODO why can't this be in property decl?
 			});
