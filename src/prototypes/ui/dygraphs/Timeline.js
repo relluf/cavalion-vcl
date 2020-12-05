71da0a36bb68829f29ee5f1198c2b85c230c3f7b
Ralph Kazemier <ralph.kazemier@gmail.com>
2020-12-04 23:59:03 -0600

    using dateWindow and valueRange, defaulting to 1Y on first render, added logging

--- src/prototypes/ui/dygraphs/Timeline.js
+++ src/prototypes/ui/dygraphs/Timeline.js
@@ -7,16 +7,29 @@ function draw(data, options) {
 /* TODO describe usage:
 	-vars: { graph, data, options }
 */
+	var scope = this.scope(), me = this;
+    var node = scope.host.getNode();
+    var history = this.vars("history");
     var graph = this.vars("graph");
+    var defs = {};
+    
     if (graph !== undefined) {
     	try {
+	    	this.print("using previous graph");
+    		defs.dateWindow = graph.xAxisRange();
+    		defs.valueRange = graph.yAxisRange();
         	graph.destroy();
     	} catch(e) {
     		console.error(e);
     	}
+    } else {
+    	this.print("no previous graph, defaulting to 1Y");
+    	defs.dateWindow = [parseInt(Date.now() / 1000) * 1000];
+    	defs.dateWindow.unshift(defs.dateWindow[0] - 365*24*60*60*1000);
     }
     
     this.clearTimeout("update-history");
+	this.print("defs", JSON.stringify(defs));
 
     if (data === undefined) {
         data = this.vars("data");
@@ -33,26 +46,13 @@ function draw(data, options) {
         this.vars("options", options);
     }
 
-    var node = this.scope().host.getNode(), me = this;
     try {
-        this.vars("graph", new Dygraph(node, data, js.mixIn({
+        this.vars("graph", new Dygraph(node, data, js.mixIn(js.mixIn({
             rollPeriod: 1,
             showRoller: true,
-            // customBars: true,
-            
-	        highlightCircleSize: 2,
-	        strokeWidth: 1,
-	        // strokeBorderWidth: isStacked ? null : 1,
-	        // labelsDivWidth: 0, ??
-	        
-			highlightSeriesOpts: {
-				strokeWidth: 3,
-				strokeBorderWidth: 1,
-				highlightCircleSize: 5
-			},
-			
 			drawCallback: function(g, is_initial) {
-				var history = me.vars("history"), scope = me.scope(), b;
+				var b;
+
 				if(history.block) {
 					if(--history.block === 0) {
 						delete history.block;
@@ -61,11 +61,10 @@ function draw(data, options) {
 					scope.history_forward.update();
 				} else {
 					me.setTimeout("update-history", function() {
-						var item = {x: g.xAxisRange(), y: g.yAxisRange()};
+						var item = { x: defs. x || g.xAxisRange(), y: defs.y || g.yAxisRange() };
 						var current = history[history.index];
 						if(item.x[0] === 0) return;
-						if(!current || (current.x[0] !== item.x[0] || current.x[1] !== item.x[1] || current.y[0] !== item.y[0] || current.y[1] !== item.y[1])
-						) {
+						if(!current || (current.x[0] !== item.x[0] || current.x[1] !== item.x[1] || current.y[0] !== item.y[0] || current.y[1] !== item.y[1])) {
 							history.splice(history.index + 1);
 							history.index = history.push(item) - 1;
 							scope.history_back.update();
@@ -73,12 +72,22 @@ function draw(data, options) {
 						}
 					}, 450);
 				}
+				me.emit("draw", [g, is_initial]);
 			},
-
-            // legend: "always",
             axisLabelFontSize: 10,
+			// highlightSeriesOpts: {
+			// 	strokeWidth: 3,
+			// 	strokeBorderWidth: 1,
+			// 	highlightCircleSize: 5
+			// },
+            // customBars: true,
+	        // highlightCircleSize: 2,
+	        // strokeWidth: 1,
+	        // strokeBorderWidth: isStacked ? null : 1,
+	        // labelsDivWidth: 0, ??
+            // legend: "always",
             // showRangeSelector: true
-        }, options || {})));
+        }, options || {}), defs)));
     } catch(e) {
     	console.error(e);
     	/*- TODO prevent construction */
@@ -86,6 +95,7 @@ function draw(data, options) {
     }
 }
 function clear() {
+	this.print("clear");
     this.clearTimeout("update-history");
     
     var graph = this.removeVar("graph");
@@ -119,10 +129,10 @@ function clear() {
     },
     onResize: function () {
         if (this.isVisible() === false) {
-// this.print("onResize -> remember");
+			this.print("onResize -> remember");
             this.vars("resized", true);
         } else {
-// this.print("onResize -> update");
+			this.print("onResize -> update", this.getComputedStyle().width);
 	        this.update(() => draw.apply(this, []));
         }
         return this.inherited(arguments);
