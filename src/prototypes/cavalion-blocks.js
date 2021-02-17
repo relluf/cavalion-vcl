67168917c2e7b5ba0263fe7fdfca6e42fdb8dd12
Ralph Kazemier <ralph.kazemier@gmail.com>
2021-02-16 20:07:00 -0600

    enhancing API

--- src/prototypes/cavalion-blocks.js
+++ src/prototypes/cavalion-blocks.js
@@ -3,8 +3,8 @@
 ["vcl/ui/Panel", {
 	align: "client",
 	onLoad() {
-		require("B").instantiate(js.sf("%s<%s>", this.getSpecializer(), this.vars("specializer") || this._name), 
-			{ owner: this }).then(_ => {
+		var uri = this.vars("uri") || js.sf("%s<%s>", this.getSpecializer(), this.vars("specializer") || this._name);
+		require("B").instantiate(uri, { owner: this }).then(_ => {
 				_.setParent(this); // TODO why can't this be in property decl?
 			});
 	}
