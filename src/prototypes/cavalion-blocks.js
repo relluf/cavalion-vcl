e0ff8f9e6e907ce02c911ce1ee64ad4b3bdc4cd8
Ralph Kazemier <ralph.kazemier@gmail.com>
2022-01-22 19:51:18 -0600

    adding extra onLoad

--- src/prototypes/cavalion-blocks.js
+++ src/prototypes/cavalion-blocks.js
@@ -4,8 +4,12 @@
 	align: "client",
 	onNodeCreated() {
 		var uri = this.vars("uri") || js.sf("%s<%s>", this.getSpecializer(), this.vars("specializer") || this._name);
+		var onLoad = this.vars("onLoad");
 		require("B").instantiate(uri, { owner: this }).then(_ => {
 				_.setParent(this); // TODO why can't this be in property decl?
+				if(onLoad) {
+					onLoad.apply(this, [_]);
+				}
 			});
 	}
 }];
