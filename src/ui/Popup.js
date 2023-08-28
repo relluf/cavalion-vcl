8ed0454cb019e69eba273d8d2ddba8d12399466f
Ralph <ralph.kazemier@gmail.com>
2023-08-28 12:56:31 -0600

    adds getControlBounds

--- src/ui/Popup.js
+++ src/ui/Popup.js
@@ -47,6 +47,25 @@ define(function(require) {
 			_onPopup: null,
 			_onClose: null,
 
+			getControlBounds: function(control) {
+				var bounds = control.getAbsoluteRect();
+				var offset = control.clientToDocument(0, 0);
+				
+				bounds.left = offset.x;
+				bounds.top = offset.y;
+
+				if(this._parent !== null) {
+					var pt = this._parent.clientToDocument(0, 0, true);
+
+					bounds.left -= pt.x;
+					bounds.top -= pt.y;
+				}
+
+				bounds.x = bounds.left;
+				bounds.y = bounds.top;
+
+				return bounds;
+			},
 			popup: function(position, relativeTo, onClose) {
 				/**
 				 *
@@ -56,24 +75,40 @@ define(function(require) {
 				 */
 				const align = () => {
 	
-						var p = relativeTo.clientToDocument(0, 0);
-						var cs = relativeTo.getComputedStyle();
-						var cs_ = this.getComputedStyle();
+					var ar = relativeTo.getAbsoluteRect();
+					var p = relativeTo.clientToDocument(0, 0);
+					var cs = relativeTo.getComputedStyle();
+					var cs_ = this.getComputedStyle();
+					var ar_ = this.getAbsoluteRect();
+					
+					var bounds = this.getControlBounds(this);
 
-						if(this._parent) {
-							p = this._parent.documentToClient(p);
-						}
+					if(this._parent) {
+						p = this._parent.documentToClient(p);
+					}
+					
+					// this.print("popup", {
+					// 	bounds: bounds,
+					// 	ar: ar,
+					// 	ar_: ar_,
+					// 	cs: cs,
+					// 	cs_: cs_
+					// });
+					
+					if(position.origin === "bottom-left") {
+						this.setLeft(p.x + (position.dx || 0));
+						this.setTop(p.y + parseInt(cs.height, 10) + (position.dy || 0));
+					} else if(position.origin === "bottom-right") {
+						var l = p.x + parseInt(cs.width, 10) + (position.dx || 0) - this._node.scrollWidth; //(parseInt(cs_.width, 10) || 0);
+						var t = p.y + parseInt(cs.height, 10) + (position.dy || 0);
 						
-						if(position.origin === "bottom-left") {
-							this.setLeft(p.x + (position.dx || 0));
-							this.setTop(p.y + parseInt(cs.height, 10) + (position.dy || 0));
-						} else if(position.origin === "bottom-right") {
-							this.setLeft(p.x + parseInt(cs.width, 10) + (position.dx || 0) - parseInt(cs_.width, 10));
-							this.setTop(p.y + parseInt(cs.height, 10) + (position.dy || 0));
-						}
-					};
+						// this.print("popup-setTopLeft", js.sf("%d %d", p.x, p.y));
+						
+						this.setLeft(l);
+						this.setTop(t);
+					}
+				};
 
-				var n = 2;
 				if(!this._hook.isActive()) {
 					
 					this.setVisible(true);
@@ -88,6 +123,7 @@ define(function(require) {
 
 					this.update(align);
 					this.onpopup();
+					this.setTimeout("update-align", align, 250);
 				}
 				
 				align();
