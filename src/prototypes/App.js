f43af78cfc9beb3658459bd190401f66472f1927
Ralph <ralph.kazemier@gmail.com>
2023-09-08 13:07:05 -0600

    adds css styles to App.v1 simplifies code

--- src/prototypes/App.js
+++ src/prototypes/App.js
@@ -1,22 +1,10 @@
-"use vcl/ui/Node, vcl/ui/Button";
+"";
 
-[("vcl/Application"), {
-	
-	onLoad() { 
-		this._components[0].setParentNode(document.body); 
-	}
-	
-}, [
-    [("vcl/ui/Panel"), "window", { 
+["vcl/Application", { }, [
+    ["vcl/ui/Panel", ("window"), { 
     	align: "client", classes: "animated",
-    	css: {
-			".{./Button}": {
-				"font-family": "Lucida Grande, Arial, sans-serif",
-				"font-size": "9pt"
-			},
-			// "font-family": "SF Pro Text, SF UI Text, system-ui, Helvetica Neue, Helvetica, Lucida Grande, Arial, sans-serif",
-			"font-family": "Lucida Grande, Arial, sans-serif",
-			"font-size": "9pt"
-    	}
+		onLoad() { 
+			this.setParentNode(document.body); 
+		}
     }]
 ]];
