be9a7f815fe421801c0f47675c960c613439d573
Ralph <ralph.kazemier@gmail.com>
2023-09-16 15:13:07 -0600

    cleans up code for readability

--- src/ui/Console.evaluate.js
+++ src/ui/Console.evaluate.js
@@ -1,38 +1,13 @@
 define(function() { return function(expr) {
-	
+
 	var Console = this.constructor;
 	var Deferred = require("js/Deferred");
 	var Component = require("vcl/Component");
+	var Control = require("vcl/Control");
+
     var scope = this.scope();
     var me = this;
 	
-	// function log(console) {
-	// 	var args = js.copy_args(arguments);
-
-	// 	/*- first param can be owner or the console */
-	// 	if(console instanceof Component) {
-	// 		var scope = console.scope();
-	// 		if(!(console instanceof Console)) {
-	// 			console = scope.console;
-	// 		}
-	// 		if(args[0] === scope['@owner']) {
-	// 			args.shift();
-	// 		}
-	// 	}
-		
-	// 	if(args.length >= 2) {
-	// 		args[0] = String.format("%n", args[0]);
-	// 		if(args.length === 3 && typeof args[1] === "string") {
-	// 			args[0] += String.format(" - " + args.splice(1, 1)[0]);
-	// 		}
-	// 	}
-		
-	// 	if(!console) {
-	// 		console = require("vcl/Application").instances[0].down("vcl/ui/Console");
-	// 	}
-		
-	// 	return console.print.apply(console, args);
-	// }
 	function req() {
 	    if (arguments.length == 1) {
 	        try {
@@ -49,8 +24,11 @@ define(function() { return function(expr) {
 	        }
 	    ]);
 	    return d;
-	}				
-	function pr() { me.print.apply(me, arguments); }
+	}
+	
+	const cl = console.log;
+	// const cc = require("clipboard-copy");
+	const pr = () => me.print.apply(me, arguments);
 
     /* jshint evil: true */
 	return eval(expr);
