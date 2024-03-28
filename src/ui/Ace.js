32f236bac95de8b56f71f8a61d119dffa017c24a
Ralph <ralph.kazemier@gmail.com>
2024-03-28 01:35:21 -0600

    adds support for marking and patching diffs

--- src/ui/Ace.js
+++ src/ui/Ace.js
@@ -1,6 +1,8 @@
-define(["require", "js/defineClass", "ace/ace", "ace/commands/default_commands", "./Panel", "js/Type"], 
-function(require, Ace, ace, DefaultCommands, Panel, Type) {
+define(["require", "js/defineClass", "ace/ace", "ace/commands/default_commands", "./Panel", "js/Type", "util/Text", "ace/Range"], 
+function(require, Ace, ace, DefaultCommands, Panel, Type, Text) {
     
+	var Range = require("ace/Range").Range;
+	
 	// Somehow this got broken Cmd+Alt+0 (really missing it, spend over 2 hours (sigh!))
 	DefaultCommands.commands[12].exec = (editor) => { 
 		editor.session.foldAll(); 
@@ -35,18 +37,33 @@ function(require, Ace, ace, DefaultCommands, Panel, Type) {
 	        }
 		});
 	};
-	
+	const removeAllMarkers = (editor, lines) => {
+	    // Haal alle markers op (zowel voor de achtergrond als voor de selectie)
+	    const markers = editor.session.getMarkers(false); // true voor achtergrond markers
+
+	    // Verwijder elke marker
+	    for (let markerId in markers) {
+	        editor.session.removeMarker(markerId);
+	    }
+	    
+		lines.forEach(lineNumber => editor
+			.session
+			.removeGutterDecoration(lineNumber, "line-deleted"));
+	};
+	const markDeletionInGutter = (editor, lineNumber) => {
+	    editor.session.addGutterDecoration(lineNumber, "line-deleted");
+	};
 
 	return (Ace = Ace(require, {
-
 		inherits: Panel,
-
 		prototype: {
 
 			"@css": {
 				"background-color": "white",
 				"z-index": "200",
-				">div": "position: absolute; top: 0; left: 0; bottom: 0; right: 0;"
+				">div": "position: absolute; top: 0; left: 0; bottom: 0; right: 0;",
+				".textdiff": "background-color: rgba(255,255,0,0.5); position: absolute;",
+				".line-deleted":"position: absolute; background-color: #ffcccc; right: 0; left: 0;"
 			},
 
 			_align: "client",
@@ -55,13 +72,6 @@ function(require, Ace, ace, DefaultCommands, Panel, Type) {
 
 			_cursorPos: null,
 
-			applyBounds: function() {
-				/**
-				 * @overrides ./Panel.prototype.applyBounds
-				 */
-				this.inherited(arguments);
-				this._editor.resize();
-			},
 			onnodecreated: function() {
 				/**
 				 * @overrides ../Control.prototype.onnodecreated
@@ -77,18 +87,26 @@ function(require, Ace, ace, DefaultCommands, Panel, Type) {
 				
 				return this.inherited(arguments);
 			},
+			onchange: function() {
+				/**
+				 *
+				 */
+				return this.fire("onChange", arguments);
+			},
+
+			applyBounds: function() {
+				/**
+				 * @overrides ./Panel.prototype.applyBounds
+				 */
+				this.inherited(arguments);
+				this._editor.resize();
+			},
 			setFocus: function() {
 				/**
 				 * @overrides ../Control.prototype.setFocus
 				 */
 				this.nodeNeeded();
 				this._editor.focus();
-			},
-			onchange: function() {
-				/**
-				 *
-				 */
-				return this.fire("onChange", arguments);
 			},
 			getEditor: function() {
 				/**
@@ -104,6 +122,9 @@ function(require, Ace, ace, DefaultCommands, Panel, Type) {
 			},
 			setValue: function(value) {
 				this.nodeNeeded();
+				if(value instanceof Array && value.every(s => typeof s === "string")) {
+					value = value.join("\n");
+				}
 				return this._editor.setValue(typeof value === "string" ? value : JSON.stringify(value));
 			},
 			getLines: function(seperator) {
@@ -111,6 +132,67 @@ function(require, Ace, ace, DefaultCommands, Panel, Type) {
 			},
 			setMode: function (mode) {
 				this.getEditor().session.setMode("ace/mode/" + mode);
+			},
+			
+			getDiffs: function(originalText) {
+				var dmp = new Text.dmp();
+				var editor = this.getEditor();
+
+			    const currentText = editor.getValue();
+			    const diffs = dmp.diff_main(originalText, currentText);
+			    dmp.diff_cleanupSemantic(diffs); // Maakt de diff output schoner
+			    
+			    return diffs;
+			},
+			markDiffs: function(diffs) {
+				var editor = this.getEditor();
+ 				if(typeof diffs === "string") {
+					return this.markDiffs(this.getDiffs(diffs));
+				}
+				
+			    removeAllMarkers(editor, this.getLines().map((l, i) => i));
+
+			    let cursorPosition = 0; // Houdt de positie in de tekst bij
+			    diffs.forEach((diff) => {
+			        const [operation, text] = diff;
+			        switch (operation) {
+			            case 0: // Geen wijziging
+			                cursorPosition += text.length;
+			                break;
+			            case -1: // Tekst verwijderd
+			                // Marker voor verwijderde tekst niet nodig, maar je kunt dit aanpassen
+			                // cursorPosition += text.length; // Verwijderde tekst overslaan
+							const p = editor.session.doc.indexToPosition(cursorPosition, 0);
+			                markDeletionInGutter(editor, p.row);
+			                break;
+			            case 1: // Tekst toegevoegd
+			                const start = editor.session.doc.indexToPosition(cursorPosition, 0);
+			                const end = editor.session.doc.indexToPosition(cursorPosition + text.length, 0);
+			                const m = editor.session.addMarker(new Range(start.row, start.column, end.row, end.column), "textdiff", "text", false);
+			                cursorPosition += text.length;
+			                break;
+			        }
+			    });
+			    
+				return diffs;
+			},
+			makePatch: function(originalText, diffs) {
+				if(typeof diffs === "undefined") {
+					diffs = this.getChanges(originalText);
+				}
+				
+				var dmp = new Text.dmp();
+			    return dmp.patch_make(originalText, diffs);
+			},
+			applyPatch: function(originalText, patch) {
+				var dmp = new Text.dmp();
+			    const results = dmp.patch_apply(patch, originalText);
+			    // originalText = results[0]; // Update de originele tekst met de nieuwe versie
+			    // console.log("patch_apply-results", results);
+			    this.setValue(results[0]);
+			},
+			removeDiffs: function() {
+			    removeAllMarkers(this.getEditor(), this.getLines().map((l, i) => i));
 			}
 		}, 
 		properties: {
