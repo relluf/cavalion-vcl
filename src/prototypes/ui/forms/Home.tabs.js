94ca0596bcf470567a620b165412f481780d22ba
Ralph <ralph.kazemier@gmail.com>
2024-06-28 21:53:38 -0600

    adds Home.tabs first commit

---
+++ src/prototypes/ui/forms/Home.tabs.js
@@ -0,0 +1,9 @@
+"use js";
+
+["", {}, [
+    [("#description"), {
+    	content: "Home.tabs: Navigeer met behulp van de UI hieronder naar de gewenste sectie."
+    }],
+    
+    ["vcl/ui/Tabs", "tabs", { classes: "white-blue" }]
+]];
